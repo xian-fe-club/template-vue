a313149134d6d28a717b53d3ed64f3c538974260
xiaobai <1041258672@qq.com>
2021-03-29 15:04:55 +0800

    feat: 修改过滤器引用

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -2,14 +2,16 @@
  * @Description:
  * @Author: liudehua
  * @Date: 2021-01-04 10:58:26
- * @LastEditTime: 2021-01-05 14:43:05
- * @LastEditors: liudehua
+ * @LastEditTime: 2021-03-29 15:00:05
+ * @LastEditors: Please set LastEditors
  */
 
-import DateFormat from "./modules/date";
-import Money from "./modules/money";
+const modulesFiles = require.context("./modules", true, /\.ts$/);
+const modules = modulesFiles.keys().reduce((modules: Record<string, any>, modulePath) => {
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, "$1");
+  const value = modulesFiles(modulePath);
+  modules[moduleName] = value.default;
+  return modules;
+}, {});
 
-export default {
-  DateFormat,
-  Money
-};
+export default modules;
