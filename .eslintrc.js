f92d56cea9fc88b2e57f76ca4d44b17f17afba5f
xiaobai <1041258672@qq.com>
2021-04-30 16:45:24 +0800

    feat: 添加规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,27 +4,27 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint'
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    '@typescript-eslint/no-explicit-any': ['off'],
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'prefer-const': 'off',
-    'no-unused-expressions': 0,
-    '@typescript-eslint/no-var-requires': 0,
-    'no-unused-vars': 0
+    "@typescript-eslint/no-explicit-any": ["off"],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prefer-const": "off",
+    "no-unused-expressions": 0,
+    "@typescript-eslint/no-var-requires": 0,
+    "no-unused-vars": 0
   },
   overrides: [
     {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
       env: {
         mocha: true
       }
