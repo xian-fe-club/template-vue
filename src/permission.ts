e4c5b80bb4b6ea52c9d5052649b9ae03574dc4c8
jianxiaobai <1041258672@qq.com>
2021-12-02 18:27:45 +0800

    feat: 修改配置

--- src/permission.ts
+++ src/permission.ts
@@ -2,51 +2,45 @@
  * @Description:
  * @Author: liudehua
  * @Date: 2021-02-07 15:29:30
- * @LastEditTime: 2021-04-01 15:43:36
+ * @LastEditTime: 2021-12-02 18:10:10
  * @LastEditors: Please set LastEditors
  */
 import router from "./router";
-import NProgress from 'nprogress'
+import NProgress from "nprogress";
 import { getToken } from "@/utils/auth";
 import store from "./store";
 
-const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
+const whiteList = ["/login", "/auth-redirect"]; // no redirect whitelist
 
-router.beforeEach(async(to: any, _: any, next: any) => {
-  NProgress.start()
-  const hasToken = getToken()
-  const hasRoles = store.getters.roles && store.getters.roles.length > 0
+router.beforeEach(async (to: any, _: any, next: any) => {
+  NProgress.start();
+  const hasToken = getToken();
+  const hasRoles = store.getters.roles && store.getters.roles.length > 0;
   if (hasToken && hasRoles) {
-    if (to.path === '/login') {
-      next({ path: '/app' })
-      NProgress.done()
+    if (to.path === "/login") {
+      next({ path: "/app" });
+      NProgress.done();
     } else {
-      next()
+      if (to.meta.title) document.title = to.meta.title;
+      next();
     }
   } else if (hasToken && !hasRoles) {
     try {
-      await store.dispatch('user/genUserInfo') // genUserInfo内部将更新路由
-      next({ ...to, replace: true })
+      await store.dispatch("user/genUserInfo"); // genUserInfo内部将更新路由
+      next({ ...to, replace: true });
     } catch (err) {
-      await store.dispatch('user/logout')
-      next(`/login?redirect=${to.path}`)
-      NProgress.done()
+      await store.dispatch("user/logout");
+      next(`/login?redirect=${to.path}`);
+      NProgress.done();
     }
   } else if (!hasToken) {
     if (whiteList.indexOf(to.path) !== -1) {
       // in the free login whitelist, go directly
-      next()
+      next();
     } else {
       // other pages that do not have permission to access are redirected to the login page.
-      next(`/login?redirect=${to.path}`)
-      NProgress.done()
+      next(`/login?redirect=${to.path}`);
+      NProgress.done();
     }
   }
-  // if (to.path === "/") {
-  //   NProgress.done()
-  //   return next("/login");
-  // }
-  // if (to.meta.title) document.title = to.meta.title;
-  // next();
-  // NProgress.done()
 });
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,9 +1,4 @@
-// declare module '*.vue' {
-//   import type { DefineComponent } from 'vue'
-//   const component: DefineComponent<{}, {}, any>
-//   export default component
-// }
-
+declare module "nprogress";
 declare module "*.vue" {
   import { defineComponent } from "vue";
   const component: ReturnType<typeof defineComponent>;
